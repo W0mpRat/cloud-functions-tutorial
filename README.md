4b9645929fdc8cff3d5a3d8ee01a5efc6dc29df7
Chase Anderson <anderson.chasew@gmail.com>
2021-06-08 09:57:43 -0600

    lesson 4

--- README.md
+++ README.md
@@ -3,7 +3,8 @@
 ### Initial Setup
 - `firebase login`
 - `firebase init`
-- `firebase serve` will serve ./public
+- `firebase serve` will serve ./public (use emulators instead)
+- `firebase emulators:start --only "hosting","functions"`
 - `firebase deploy`
 
 ![image](https://user-images.githubusercontent.com/3271894/121212395-8c27a980-c83a-11eb-9f7c-c4265c1bfcf8.png)
--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,26 @@
 const functions = require("firebase-functions");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+// Create and Deploy Your First Cloud Functions
+// https://firebase.google.com/docs/functions/write-firebase-functions
+
+exports.helloWorld = functions.https.onRequest((request, response) => {
+  functions.logger.info("Hello logs!", {structuredData: true});
+  response.send("Hello from Firebase!");
+});
+
+// http request 1
+exports.randomNumber = functions.https.onRequest((req, res) => {
+  const number = Math.round(Math.random() * 100);
+
+  functions.logger.info(number, {structuredData: true});
+
+  console.log(number);
+
+  res.send(number.toString());
+});
+
+
+// http request 2
+exports.toTheDojo = functions.https.onRequest((req, res) => {
+  res.redirect("https://www.thenetninja.co.uk");
+});
