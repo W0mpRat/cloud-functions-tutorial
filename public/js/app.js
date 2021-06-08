e1bf7aa6926084433e7b1f45809de9377cbe7128
Chase Anderson <anderson.chasew@gmail.com>
2021-06-08 14:31:18 -0600

    lesson 11

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,6 @@
 const requestModal = document.querySelector('.new-request');
 const requestLink = document.querySelector('.add-request');
+const requestForm = document.querySelector('.new-request form');
 
 function init() {
   firebase.functions().useEmulator('localhost', 5001)
@@ -17,6 +18,24 @@ requestModal.addEventListener('click', (e) => {
   if (e.target.classList.contains('new-request')) {
     requestModal.classList.remove('open');
   }
+});
+
+// add a new request
+requestForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const addRequest = firebase.functions().httpsCallable('addRequest');
+  addRequest({ 
+    requestString: requestForm.request.value 
+  })
+  .then(() => {
+    requestForm.reset();
+    requestForm.querySelector('.error').textContent = '';
+    requestModal.classList.remove('open');
+  })
+  .catch(error => {
+    requestForm.querySelector('.error').textContent = error.message;
+  });
 });
 
 // say hello function call
